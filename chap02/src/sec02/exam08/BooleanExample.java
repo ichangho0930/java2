0b76d41f143131732456fb8fa6d68dd522cfaacd
ichangho0930 <szffddx@naver.com>
2025-08-27 09:56:21 +0900

    First Commit

---
+++ chap02/src/sec02/exam08/BooleanExample.java
@@ -0,0 +1,15 @@
+package sec02.exam08;
+
+public class BooleanExample {
+
+	public static void main(String[] args) {
+		boolean stop = true;
+		if (stop) {
+			System.out.println("중지되었습니다");
+		}
+		else {
+			System.out.println("시작합니다.");
+		}
+	}
+
+}
