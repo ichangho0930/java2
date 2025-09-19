3aae0aace79d6692fb80d16faf12ad788fd4d364
ichangho0930 <szffddx@naver.com>
2025-09-19 11:19:26 +0900

    9월 19일 이창호

---
+++ chap06/src/sec05/exam04/Singleton.java
@@ -0,0 +1,21 @@
+package sec05.exam04;
+
+public class Singleton {
+	/*private static Singleton singleton = new Singleton();
+	
+	private Singleton() {}
+	
+	static Singleton getInstance() {
+		return singleton;
+	}*/
+	
+	private static Singleton singleton = null;
+	
+	private Singleton() {}
+	
+	public static Singleton getInstance() {
+		if (singleton == null)
+			singleton = new Singleton();
+		return singleton;
+	}
+}
